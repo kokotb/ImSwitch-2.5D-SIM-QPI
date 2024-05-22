ae4b20d498a81071918d7e3b34f4757b159c0d53
kokotb <169988194+kokotb@users.noreply.github.com>
2024-05-22 12:00:40 +0200

    Resolving conflicts between master and a branch.

--- imswitch/imcontrol/controller/MasterController.py
+++ imswitch/imcontrol/controller/MasterController.py
@@ -67,6 +67,9 @@ class MasterController:
         self.detectorsManager.sigDetectorSwitched.connect(cc.sigDetectorSwitched)
         self.detectorsManager.sigImageUpdated.connect(cc.sigUpdateImage)
         self.detectorsManager.sigNewFrame.connect(cc.sigNewFrame)
+        self.detectorsManager.sigRunAcquireSetStarted.connect(cc.sigRunAcquireSetStarted)
+        self.detectorsManager.sigRunAcquireSetStopped.connect(cc.sigRunAcquireSetStopped)
+        
 
         self.recordingManager.sigRecordingStarted.connect(cc.sigRecordingStarted)
         self.recordingManager.sigRecordingEnded.connect(cc.sigRecordingEnded)
