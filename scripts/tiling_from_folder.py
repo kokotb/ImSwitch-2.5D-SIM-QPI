5da09d87fef814225db39298071396977353ced0
kokotb <169988194+kokotb@users.noreply.github.com>
2024-07-12 15:25:13 +0200

    Tiling script development. Version in between.

--- scripts/tiling_from_folder.py
+++ scripts/tiling_from_folder.py
@@ -71,6 +71,35 @@ def create_tiling_from_tif(tiling_paths, num_columns, num_rows, overlay):
     return final_tiling
 
 
+def getUniqueNamesByPattern(file_names, pattern):
+    all_names_unique = []
+    # Split by time
+    for name in file_names:
+        start = name.find(pattern)
+        if start != -1:
+            new = name[0:start + len(pattern) + 4]
+            all_names_unique.append(new)
+    # List of unique names for time points
+    all_names_unique = list(set(all_names_unique))
+    all_names_unique = sorted(all_names_unique)
+    num_names = len(all_names_unique)
+    return num_names, all_names_unique
+
+
+def getNamesByPattern(file_names, pattern):
+    all_names_unique = []
+    # Split by time
+    for name in file_names:
+        start = name.find(pattern)
+        if start != -1:
+            all_names_unique.append(name)
+    # List of unique names for time points
+    all_names_unique = list(set(all_names_unique))
+    all_names_unique = sorted(all_names_unique)
+    num_names = len(all_names_unique)
+    return num_names, all_names_unique
+
+
 ##############################
 #      SET PARAMETERS        #
 ##############################
@@ -78,6 +107,7 @@ input_dir = "D:\\Documents\\4 - software\\python-scripting\\2p5D-SIM\\test_expor
 exp_names = ["2024_07_11-04-15-53_PM"]
 single_channels_names = ['488nm', '561nm', '640nm']
 name_pattern = "Reconstruction" # can be wf or something else
+t_pattern = "frame_"
 number_of_rows = 5
 number_of_columns = 5
 
@@ -113,25 +143,92 @@ except OSError as error:
 #TODO: Make an image overlay in python
 
 
-
 # Perform tiling
 if create_tiling:
+    # Repeat for each 
     for exp_name in exp_names:
         # Get all tif files
-        all_paths = glob.glob(input_dir + f'\\{exp_name}*.tif')
+        all_paths = glob.glob(input_dir + f'\\{exp_name}*{name_pattern}*.tif')
         all_names = [os.path.basename(x) for x in all_paths]
         all_names_unique = []
         
-        # Get all the unique names (positions), needs changing if the name structure is different!!!!
-        for part in all_names:
-            start = part.find(name_pattern)
-            new = part[0:start + len(name_pattern) + 1]
-            all_names_unique.append(new)
-        # list of unique names
-        all_rois = list(set(all_names_unique))
-        all_rois = sorted(all_rois)
-        num_rois = len(all_rois)
-    
+        
+        
+        if all_names == []:
+            print(f"{exp_name} not present in folder. Skipping analysis.")
+            continue
+        
+        # # Split by time
+        # for part in all_names:
+        #     start = part.find(t_pattern)
+        #     new = part[0:start + len(t_pattern) + 4]
+        #     all_names_unique.append(new)
+        # # List of unique names for time points
+        # all_times = list(set(all_names_unique))
+        # all_times = sorted(all_times)
+        # num_times = len(all_times)
+        
+        num_times, all_times = getUniqueNamesByPattern(all_names, t_pattern)
+        
+        # Do each chan seprately
+        for ch in single_channels_names:
+            # num_rois_times, all_rois_times = getUniqueNamesByPattern(all_names, ch)
+            # Do each time point separately
+            for num_time, name_time in enumerate(all_times):
+                roi_names_unique = []
+                # Get all unique ROI names for this time point
+                # num_rois, all_rois = getNamesByPattern(all_rois_times, name_time)
+                
+                roi_names_import = glob.glob(f'{input_dir}\\{name_time}*{ch}*.tif')
+                # for roi in all_rois:
+                #     roi_names_import = glob.glob(f'{input_dir}\\{name_time}*{ch}.tif')
+                #     roi_names_import.append(all_rois_org[0])
+                # for roi in all_rois: 
+                #     num, all_rois_org = getNamesByPattern(all_names, roi)
+                #     roi_names_import.append(all_rois_org[0])
+                print("")
+                # for part in all_names: 
+                #     is_part = part.find(name_time)
+                #     if is_part != -1:
+                #         start = part.find(name_pattern)
+                #         new = part[0:start + len(name_pattern)]
+                #         roi_names_unique.append(new)
+                # all_rois = list(set(roi_names_unique))
+                # all_rois = sorted(all_rois)
+                # num_rois = len(all_rois)
+                
+                # Tile each chan
+                chan_names_unique = []
+                for chan in single_channels_names:
+                    # Get name patterns
+                    for part in all_names: 
+                        is_part = part.find(chan)
+                        if is_part != -1:
+                            start = is_part
+                            new = part[0:start + len(chan)]
+                            chan_names_unique.append(new)
+                    all_rois_chan = list(set(chan_names_unique))
+                    all_rois_chan = sorted(all_rois_chan)
+                    num_rois_chan = len(all_rois_chan)
+
+                    print("")
+        # # Get all the unique names (positions), needs changing if the name structure is different!!!!
+        # for part in all_names:
+        #     start = part.find(name_pattern)
+        #     new = part[0:start + len(name_pattern) + 1]
+        #     all_names_unique.append(new)
+        # # List of unique names - channels omitted
+        # all_times_rois = list(set(all_names_unique))
+        # all_times_rois = sorted(all_times_rois)
+        # num_times_rois = len(all_times_rois)
+        
+
+        
+        # for i, roi in enumerate(all_times_rois):
+        #     print(f'----ROI{i+1} out of {num_times_rois}----')
+        #     print(f'{roi} \n')
+        #     roi_times = glob.glob(input_dir + f'\\{roi}*.tif')
+        # print(roi_times)
     # print('\ncreating panoramas ...')
     # tiling_from = [input_dir]
     # endings = ['Reconstruction_']
