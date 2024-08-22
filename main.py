cf993ff08128653a1569d9b8dd7b2bfe483324de
kokotb <169988194+kokotb@users.noreply.github.com>
2024-08-22 11:22:35 +0200

    Editing main.py to remove only one .json file that enables us to choose the config file at startup.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from imswitch.__main__ import main
 import shutil
-shutil.rmtree('C:/Users/SIM_admin/Documents/ImSwitchConfig')  
-# shutil.rmtree('C:/Users/Bostjan Kokot/Documents/ImSwitchConfig')
+import os
+
+# Deletes only file needed to be able to choose config file at startup
+# os.remove('C:/Users/SIM_admin/Documents/ImSwitchConfig/config/imcontrol_options.json')
+os.remove('C:/Users/Bostjan Kokot/Documents/ImSwitchConfig/config/imcontrol_options.json')
+# os.remove('C:/Users/Administrator/Documents/ImSwitchConfig/config/imcontrol_options.json')
+
+# FIXME: Remove if .json is confirmed to work well
+# Deletes whole config folder
+# shutil.rmtree('C:/Users/SIM_admin/Documents/ImSwitchConfig')  
+# shutil.rmtree('C:/Users/Bostjan Kokot/Documents/ImSwitchConfig/config/')
 # shutil.rmtree('C:/Users/Administrator/Documents/ImSwitchConfig')
 main()
