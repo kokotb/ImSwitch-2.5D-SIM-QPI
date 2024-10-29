3cfdc486470afec958fb9a9dcc5201ba002533fa
CFTripp <170081779+CFTripp@users.noreply.github.com>
2024-10-29 11:41:49 +0100

    Moved more unused files.

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from imswitch.__main__ import main
 import shutil
 import os
 
-
-
-
 # Deletes only file needed to be able to choose config file at startup
 # os.remove('C:/Users/SIM_admin/Documents/ImSwitchConfig/config/imcontrol_options.json')
 # os.remove('C:/Users/Bostjan Kokot/Documents/ImSwitchConfig/config/imcontrol_options.json')
@@ -12,9 +9,9 @@ import os
 
 # FIXME: Remove if .json is confirmed to work well
 # Deletes whole config folder
-# shutil.rmtree('C:/Users/SIM/Documents/ImSwitchConfig') 
+# shutil.rmtree('C:/Users/SIM/Documents/ImSwitchConfig')
 # shutil.rmtree('C:/Users/SIM_admin/Documents/ImSwitchConfig')
 # shutil.rmtree('C:/Users/Bostjan Kokot/Documents/ImSwitchConfig/config/')
-# shutil.rmtree('C:/Users/Bostjan Kokot/Documents/ImSwitchConfig/') 
+# shutil.rmtree('C:/Users/Bostjan Kokot/Documents/ImSwitchConfig/')
 # shutil.rmtree('C:/Users/Administrator/Documents/ImSwitchConfig')
 main()
