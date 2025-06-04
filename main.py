ec1f99a2fb877e824b1a812b37acafd819ddd226
CFTripp <170081779+CFTripp@users.noreply.github.com>
2025-06-04 10:44:19 +0200

    Last commit before attempting scatter cam

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 # FIXME: Remove if .json is confirmed to work well
 # Deletes whole config folder
 # shutil.rmtree('C:/Users/SIM/Documents/ImSwitchConfig')
-# shutil.rmtree('C:/Users/SIM_admin/Documents/ImSwitchConfig')
+shutil.rmtree('C:/Users/SIM_admin/Documents/ImSwitchConfig')  
 # shutil.rmtree('C:/Users/Cody/Documents/ImSwitchConfig')
 # shutil.rmtree('C:/Users/Bostjan Kokot/Documents/ImSwitchConfig/')
 # shutil.rmtree('C:/Users/Administrator/Documents/ImSwitchConfig')
@@ -20,3 +20,4 @@ main()
 
 
 
+  
