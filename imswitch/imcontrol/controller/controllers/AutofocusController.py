1acd0d146c6d0b21e6eee6b361a7707bd81cb4c8
CFTripp <170081779+CFTripp@users.noreply.github.com>
2025-06-23 16:03:44 +0200

    Slope and intercept for lin reg curve done

--- imswitch/imcontrol/controller/controllers/AutofocusController.py
+++ imswitch/imcontrol/controller/controllers/AutofocusController.py
@@ -5,7 +5,7 @@ import os
 import cv2
 import tifffile as tif
 import threading
-
+import matplotlib.pyplot as plt
 import time
 
 class AutofocusController(ImConWidgetController):
@@ -26,6 +26,7 @@ class AutofocusController(ImConWidgetController):
 
         self.zPositioner = self._master.positionersManager._subManagers['Z']
         self.AFCam = self._master.detectorsManager._subManagers['AF Cam']
+        self.calCurveImgs = []
 
 
     def resetROIOnCam(self):
@@ -53,21 +54,117 @@ class AutofocusController(ImConWidgetController):
         return img
     
     def runCalCurve(self):
-        path = R'D:\SIM_Data\_Settings\AFImages'
+        # path = R'D:\SIM_Data\_Settings\AFImages'
         zList, currentZ = self.calcZRange()
         # zList = zList.reverse()
+        if len(self.calCurveImgs) != 0:
+            self.calCurveImgs = []
         for count, z in enumerate(zList):
-            filename = f"{count:03}.tif"
+            # filename = f"{count:03}.tif"
             self.zPositioner.setPosition(zList[count], 'Z')
             time.sleep(0.001)
             img = self.getOneFrame()
-            self.saveImageInBackground(img, path, filename)
+            # self.saveImageInBackground(img, path, filename)
+            self.calCurveImgs.append(img)
         self.zPositioner.setPosition(currentZ, 'Z')
         print('AF stack saved.')
+        self.scoreCalCurveImgs(zList)
+
+    def scoreCalCurveImgs(self, zList):
+        Range = len(zList)  	# Number of files
+        zval = 0.8    	# Step size in microns
+        lowZ = 204
+
+        # Define the model function. In our case, a 1D Gaussian.
+        def Gaussian1D(xdata, i0, x0, sX, amp):
+            x = xdata
+            x0 = float(x0)
+            eq = i0+amp*np.exp(-((x-x0)**2/2/sX**2))
+            return eq
+
+        try:
+            from scipy.optimize import curve_fit
+        except ImportError:
+            print("Unable to import curve_fit from scipy.optimize.")
+
+
+        init_guess_x = [0,80,10,62]	# Guesses for fits Background, Centre, Width, Amplitude
+        init_guess_y = [0,80,10,31]	# Guesses for fits
+        x_c = []
+        x_sigma = []
+        y_sigma = []
+        i_values = []
+
+        # To read the acquired images and apply the Gaussian fitting
+        for i in range(len(self.calCurveImgs)):
+            i_values.append(i)
+            print("Step " + str(i+1) + " of " + str(Range))
+            #Reading the frames
+            im = self.calCurveImgs[i]
+            # img = cv2.imread(stacks,-1)
+            # im = np.asarray(img).astype(float)
+            im = im-np.mean(im)/2	# Remove background
+            im[im<10] = 0			# Threshold
+        
+            # 1D Gaussian
+            h1, w1 = im.shape
+            x = np.arange(w1)
+            y = np.arange(h1)
+            
+            # Do x fit
+            popt, pcov = curve_fit(Gaussian1D, x, np.mean(im,axis=0), p0=init_guess_x, maxfev = 50000)
+            x0 = popt[1]
+            sx = popt[2]
+            init_guess_x.clear()
+            init_guess_x.append(popt)
+            # Do y fit
+            popt, pcov = curve_fit(Gaussian1D, y, np.mean(im,axis=1), p0=init_guess_y, maxfev = 50000)
+            y0 = popt[1]
+            sy = popt[2]
+            
+            # Replaces initial guess with final guess
+            init_guess_y.clear()
+            init_guess_y.append(popt)
+        
+            x_sigma.append(sx)
+            y_sigma.append(sy)
+            x_c.append(popt[1])
+            
+        # This is just to set the x-axis of the graph to the axial values
+        StepSize = zval
+        i_values = np.array(i_values)
+        z_values = zList
+        comboData = np.subtract(x_sigma,y_sigma)
+
+        # Save calibration data
+        # plt.plot(z_values, x_sigma, 'b8', markersize=2, label="σx")
+        # plt.plot(z_values, y_sigma, 'r8', markersize=2, label="σy")
+        plt.plot(np.subtract(x_sigma,y_sigma),z_values,  '--k', markersize=2, label="σx - σy")
+        # plt.grid(True)
+        plt.ylabel("z-Position (µm)")
+        plt.xlabel("Width (px)")
+        plt.legend()
+        plt.show()
+        self.y_int, self.slp = self.estimate_coef(comboData, z_values)
 
 
 
+    def estimate_coef(self, x, y):
+        # number of observations/points
+        n = np.size(x)
 
+        # mean of x and y vector
+        m_x = np.mean(x)
+        m_y = np.mean(y)
+
+        # calculating cross-deviation and deviation about x
+        SS_xy = np.sum(y*x) - n*m_y*m_x
+        SS_xx = np.sum(x*x) - n*m_x*m_x
+        # calculating regression coefficients
+        b_1 = SS_xy / SS_xx
+        b_0 = m_y - b_1*m_x
+
+        return (b_0, b_1)
         
 
     def calcZRange(self):
