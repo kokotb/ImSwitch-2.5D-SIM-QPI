8050d8563abac75cebd7b4cdc412b77a63439d14
CFTripp <170081779+CFTripp@users.noreply.github.com>
2026-02-04 10:18:11 +0100

    Reset button need update command

--- imswitch/imcontrol/view/ImConMainView.py
+++ imswitch/imcontrol/view/ImConMainView.py
@@ -58,13 +58,13 @@ class ImConMainView(QtWidgets.QMainWindow):
         rightDockInfos = {
             'Laser': _DockInfo(name='Laser Control', yPosition=0),
             'Positioner': _DockInfo(name='Positioner', yPosition=1),
-            'PSFAnalysis': _DockInfo(name = 'PSF', yPosition = 3),
-            'InfoGathering': _DockInfo(name = 'Info', yPosition = 4),
-            'ZStack': _DockInfo(name='Z-Stack', yPosition=5),         
-            'Tiling': _DockInfo(name='Tiling', yPosition=6),
-            'Timing': _DockInfo(name='Timing', yPosition=7),
-            'Autofocus': _DockInfo(name='Autofocus', yPosition=8),
-            'ROI': _DockInfo(name = 'ROI List', yPosition = 9)
+            'Autofocus': _DockInfo(name='Autofocus', yPosition=2),
+            'ROI': _DockInfo(name = 'ROI List', yPosition = 3),
+            'Tiling': _DockInfo(name='Tiling', yPosition=3),
+            'Timing': _DockInfo(name='Timing', yPosition=4),
+            'ZStack': _DockInfo(name='Z-Stack', yPosition=5),
+            'InfoGathering': _DockInfo(name = 'Info', yPosition = 6),
+            'PSFAnalysis': _DockInfo(name = 'PSF', yPosition = 7)
 
             
 
@@ -134,7 +134,7 @@ class ImConMainView(QtWidgets.QMainWindow):
         if 'Image' in self.docks:
             self.docks['Image'].setStretch(15, 1)
         if 'ZStack' in self.docks:
-            self.docks['ZStack'].setStretch(1, 10)
+            self.docks['ZStack'].setStretch(1, 0.5)
         if 'Positioner' in self.docks:
             self.docks['Positioner'].setStretch(1, 0.6)
         if 'Timing' in self.docks:
