e7f59027c5369a53586e1468cef91c5a75ab62bc
CFTripp <170081779+CFTripp@users.noreply.github.com>
2026-02-10 11:51:33 +0100

    small changes

--- imswitch/imcontrol/controller/controllers/SIMController.py
+++ imswitch/imcontrol/controller/controllers/SIMController.py
@@ -38,6 +38,10 @@ class SIMController(ImConWidgetController):
         self.isRecordWF = False
         self.isRecordRecon = False
         self.tilePreview = False
+        # self.initSaveRecon = True
+        # self.initSaveWF = False
+        self.SIMActive = False
+        self.active25D = False
 
      
         # Only napari implemented as of 12/9/24
@@ -588,7 +592,7 @@ class SIMController(ImConWidgetController):
             if (self.isReconstruction):
                 # Pass shared attributes to SIMprocessor
                 processor.setCurrentSharedAttrs(self._commChannel.sharedAttrs)
-                processor.reconstructSIMStackBackgroundLBF()
+                processor.reconstructSIMStack()
 
             if self.tilePreview and self.isTiling:
                 # if self.j == 0 and k == 0: #PROBLEM: Tiling contrast changes all channels as channels are stacked in one layer per position.
@@ -898,7 +902,7 @@ class SIMController(ImConWidgetController):
         )
         if selectedFolder:
             self._widget.path_edit.setText(selectedFolder)
-            print("Selected:", selectedFolder)
+            print("Active folder set:", selectedFolder)
 
     def confirmCreateDirectory(self, parent, path):
         reply = QMessageBox.question(
@@ -1730,7 +1734,8 @@ class SIMController(ImConWidgetController):
         # signal activates this function. Use it to break AZ loop if neccessary
 
     def sendAZFrameCoordsTo25DController(self):
-        self.stop25D()
+        if (self.SIMActive or self.active25D):
+            self.stop25D()
         for layer in self._widget.viewer.layers:
             if layer._name == 'Shapes':
                 selected_frame = layer.corner_pixels # np.array((z,y,x) top left, (z,y,x) bottom right)
@@ -1738,7 +1743,8 @@ class SIMController(ImConWidgetController):
 
 
     def sendFrameCoordsToMaskCenterLoop(self):
-        self.stop25D()
+        if (self.SIMActive or self.active25D):
+            self.stop25D()
         for layer in self._widget.viewer.layers:
             if layer._name == 'Shapes':
                 selected_frame = layer.corner_pixels # np.array((z,y,x) top left, (z,y,x) bottom right)
