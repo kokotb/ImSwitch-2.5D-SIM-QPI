69b91003882eae5c3b7458434db670a7a8c65c22
CFTripp <170081779+CFTripp@users.noreply.github.com>
2026-03-11 16:29:53 +0100

    SLM SIM mocker fixed

--- imswitch/imcontrol/model/managers/SLM4DDManagerMock.py
+++ imswitch/imcontrol/model/managers/SLM4DDManagerMock.py
@@ -44,7 +44,7 @@ class SLM4DDManagerMock(SignalInterface):
         }
 
         path = SIMSLMInfo.path
-        port = SIMSLMInfo.port
+        port = 'COM28' #dummy com port, doesnt matter
         self.slmDLL = self.getSLMDLL(path)
         self.openSLM(port)
 
